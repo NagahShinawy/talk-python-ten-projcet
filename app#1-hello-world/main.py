471c577ff949d91a505d60ebe62a82df063e0ccb
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-04-08 17:05:29 +0300

    app#1 hello world app

---
+++ app#1-hello-world/main.py
@@ -0,0 +1,30 @@
+import os
+import re
+from app import app
+BASEDIR = os.getcwd().split("\\")[-1]
+
+
+def get_username():
+    return input("What is Your Name? ")
+
+
+def validate_username(username):
+    matches = re.findall(r'^[a-zA-Z]+$', username)
+    if matches:
+        return matches[0]
+    raise ValueError(f"Invalid username <{username}>. no numbers, symbols")
+
+
+def welcome(username):
+    return f"Welcome {username.title()}!"
+
+
+def main():
+    username = get_username()
+    validated = validate_username(username)
+    print(welcome(validated))
+
+
+if __name__ == '__main__':
+    app(BASEDIR)
+    main()
