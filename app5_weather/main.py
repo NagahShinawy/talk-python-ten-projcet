1e3d81e3e27ac0ad63aa893fb60cf631525a6a67
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-05-10 14:53:20 +0300

    add header, get zipcode from user & validation

--- app5_weather/main.py
+++ app5_weather/main.py
@@ -1,22 +1,35 @@
 """
 created by Nagaj at 10/05/2021
 """
-import requests
-import bs4
-from collections import namedtuple
+import re
+
+
+def print_the_header():
+    print("-" * 70)
+    print(" " * 30 + "WEATHER APP")
+    print("-" * 70)
+
+
+def get_zipcode_from_user():
+    zipcode = input("Type Zipcode which you want weather for (97201)? ")
+    while not validate_zipcode(zipcode):
+        zipcode = input(f"Invalid zipcode '{zipcode}'. Type Again? ")
+    return zipcode
+
+
+def validate_zipcode(zipcode):
+    codes = re.findall(r"^[0-9]{5}(?:-[0-9]{4})?$", zipcode)
+    return True if codes else False
 
 
 def main():
-    # todo : print the header.
+    print_the_header()
     # todo : get the zipcode from user
+    get_zipcode_from_user()
     # todo : get html from web
     # todo: parse the html
     # todo: display for the forecast.
-    print("Hello From Main")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    names = ["john", "james", "sara", "leon", "smith"]
-    john = [name for name in names if name == 'john']   # type 'compli' for generate list comp
-    print(john)
