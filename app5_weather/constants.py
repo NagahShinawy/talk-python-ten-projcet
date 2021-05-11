a18d46e31e23a407926f024b19929242b1ea0eeb
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-05-11 14:05:21 +0300

    css selectors, get_weather_from_html, test namedtupled

--- app5_weather/constants.py
+++ app5_weather/constants.py
@@ -6,7 +6,10 @@ created by Nagaj at 10/05/2021
 
 WEATHER_ENDPOINT = "https://www.wunderground.com/weather-forecast/{zipcode}"
 
-
 # ##### CSS SELECTOR ###############################
 
 
+LOCATION_CSS = 'location h1'
+WEATHER_SCALE_CSS = '.wu-unit-temperature .wu-label'
+WEATHER_TEMP_CSS = '.wu-unit-temperature .wu-value'
+WEATHER_CONDITION_CSS = '.condition-icon'
--- app5_weather/main.py
+++ app5_weather/main.py
@@ -1,11 +1,19 @@
 """
 created by Nagaj at 10/05/2021
 """
+from collections import namedtuple
 from http import HTTPStatus
 
 import requests
+from bs4 import BeautifulSoup
 
-from constants import WEATHER_ENDPOINT
+from constants import (
+    WEATHER_ENDPOINT,
+    LOCATION_CSS,
+    WEATHER_SCALE_CSS,
+    WEATHER_CONDITION_CSS,
+    WEATHER_TEMP_CSS,
+)
 from validations import validate_zipcode
 
 
@@ -30,14 +38,43 @@ def get_html_from_web(validated_zipcode):
     return response.text
 
 
+def clean_up(text: str):
+    if not text:
+        return text
+    return text.strip()
+
+
+def get_weather_from_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    # print(soup)
+    elements = {
+        "location": LOCATION_CSS,
+        "condition": WEATHER_CONDITION_CSS,
+        "temp": WEATHER_TEMP_CSS,
+        "scale": WEATHER_SCALE_CSS,
+    }
+    for element in elements:
+        elm = soup.find(element)
+        if elm is not None:
+            text = elm.get_text()
+        else:
+            text = element + "-value"
+        clean_up(text)
+        elements[element] = text
+    print(elements)
+
+
 def main():
     print_the_header()
     validated_zipcode = get_zipcode_from_user()
     html = get_html_from_web(validated_zipcode)
+    if html is not False:
+        get_weather_from_html(html)
+    else:
+        print("SOMETHING WENT WRONG, TRY PLEASE TRY AGAIN.")
 
 
-if __name__ == "__main__":
-    # main()
+def primes_with_slices():
     primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
     #         0  1  2  3  4   5   6   7  8
     first_prime = primes[0]  # 2
@@ -52,3 +89,18 @@ if __name__ == "__main__":
     print(middle)
     last_4 = primes[-4:]  # 13, 17, 19, 23
     print(last_4)
+
+
+if __name__ == "__main__":
+    # main()
+    # primes_with_slices()
+    WeatherReport = namedtuple(
+        "WeatherReport", "location, condition, temp, scale"
+    )  # define class with attrs
+    egypt = WeatherReport("Egypt", "Overcast", "20", "C")  # init class values
+    print(egypt)
+    print(
+        f"Location is: '{egypt.location}' , Condition is: '{egypt.condition}', Temp is: '{egypt.temp}', Scale is: '{egypt.scale}'"
+    )
+    for value in egypt:
+        print(value)
